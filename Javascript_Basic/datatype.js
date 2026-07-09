73e8842bbf2a75ab2a84ef47ca5617f152ebce02
Shantanu Jumale <shantanujumale92@gmail.com>
2026-07-09 22:15:24 +0530

    Add files via upload

---
+++ Javascript_Basic/datatype.js
@@ -0,0 +1,37 @@
+console.log('I Love Teena');
+
+// let a = 10;
+// let b = a;
+// a = a + 2;
+// console.log('a : ',a);
+// console.log('b : ',b);
+
+// let u;
+
+// let sheryjs = {
+//     model : 'shantanu'
+// }
+
+// sheryjs.uid = 1; 
+
+// console.log(sheryjs);
+
+// let i = Number.MAX_SAFE_INTERGER;
+// console.lof(i);
+
+// let a = 2121211211n;   //----> big interger value at last n
+
+let a = [10,12,13,14,15,26];
+console.log(a);
+
+let studentName = "Shantanu";   // String
+let marks = 85;                 // Number
+let passed = true;              // Boolean
+let subjects = ["Math", "CS"];  // Array
+let details = { age: 25, city: "Kamthi" }; // Object
+
+console.log(studentName, marks, passed, subjects, details);
+
+
+
+
