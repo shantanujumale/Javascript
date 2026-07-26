9c2188f90ceeecb2bc4274284a8e3e83f6ad37a9
Shantanu Jumale <shantanujumale92@gmail.com>
2026-07-26 23:50:28 +0530

    Add files via upload

---
+++ Javascript_Basic/01_script.js
@@ -0,0 +1,43 @@
+// var let const
+
+// var username = 'shantanu';
+// var a = 10;
+// let a=13;
+
+
+// var a;  //declare karna
+// var a = 12 // declaration and initilization
+// window meain add karta hai
+// function scoped hota hai
+//aap firse declare kr sakte hai and error nahi honga
+
+
+// let x = 14
+// console.log(x);
+// let t = 'I Love Teena...'
+// console.log(t)
+
+
+// let age = 13;
+// console.log(age);
+
+// var x = 1;
+// console.log(x);
+// {
+//     var x = 2;
+
+// }
+
+// console.log(x);
+
+//     let x = 1;
+// {
+//     let x = 2;
+//     console.log('inside:',x);
+
+// }
+// let a = 10;
+// console.log(a);
+// console.log('outside:',x);
+
+
