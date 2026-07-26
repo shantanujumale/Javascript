f2a821a20d873a590fbad3f53f02e8b3aec96c18
Shantanu Jumale <shantanujumale92@gmail.com>
2026-07-26 23:50:05 +0530

    Add files via upload

---
+++ Javascript_Basic/02_datatype.js
@@ -0,0 +1,80 @@
+console.log('I Love Teena...  ');
+
+// let a = 10;
+// let b = a;
+// a = a + 2;
+// console.log('a : ',a);
+// console.log('b : ',b);
+
+// let u;
+
+// let sheryjs = {
+//     model : 'shantanu'
+// }
+
+// sheryjs.uid = 1; 
+
+// console.log(sheryjs);
+
+// let i = Number.MAX_SAFE_INTERGER;
+// console.lof(i);
+
+// let a = 2121211211n;   //----> big interger value at last n
+
+// let a = [10,12,13,14,15,26];
+// console.log(a);
+
+// let studentName = "Shantanu";   // String
+// let marks = 85;                 // Number
+// let passed = true;              // Boolean
+// let subjects = ["Math", "CS"];  // Array
+// let details = { age: 25, city: "Kamthi" }; // Object
+
+// console.log(studentName, marks, passed, subjects, details);
+
+// let a = "Teena";
+// a = a + '...';
+// console.log(a);
+
+// a = 10;
+// console.log('a : ',a);
+// let b = null;
+// console.log('b : ',b);
+
+// let point = 100;
+
+// let status = point > 120 ? 'Gold' : point > 50 ? 'Silver' : 'Broze';
+// console.log(status)
+
+
+// for (let i=1;i<10;i++){
+//     console.log(i+ " Hello Teena...");
+// }
+
+// let n = 0;
+// while(8>n){
+//     console.log('Hello Teena...');
+//     n++;
+// }
+
+// find the sum of number from 1 to 50 using loop
+// let sum =  0;
+// for(let i=0;i<50;i++){
+//     sum = sum + i;
+// }
+
+// console.log("sum of 1 to 50 is : "+sum);
+
+
+// print  all the number  1 to 500 that divisible by 3
+// let count=0;
+// for (let i=0;i<500;i++){
+//     if(i%12===0){
+//     console.log(i);
+//     count = count + 1;
+    
+//     }
+// }
+// console.log("count is : "+count);
+
+
