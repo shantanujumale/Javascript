cbdda19738320c66229c654e8bf0aefa1f663572
Shantanu Jumale <shantanujumale92@gmail.com>
2026-07-26 23:51:08 +0530

    Add files via upload
    3

---
+++ Javascript_Basic/03_array.js
@@ -0,0 +1,88 @@
+console.log("Hello Teena");
+
+// let arr = [1,2,3,4];
+// console.log(arr);
+
+
+// let arr = [1,2,3,4];
+// arr.push(5);   // ----> arr.push is method
+// console.log("push operation"+arr);
+
+// arr.pop(5);
+// console.log('pop operation'+arr);
+
+// arr.shift()
+// console.log('shift  operation '+arr);  // remove from first
+
+
+// let arr = [1,2,3,4,5];
+// arr.splice(2,2)
+// console.log(arr);
+
+// let arr = [11,22,33,44,55];
+
+// let newarr = arr.map(function (val){
+//     if (val>15) return val;
+// });
+
+// console.log(newarr);
+
+// let newarr = arr.filter(function(val){
+//     if(val>4) return true;
+// });
+// console.log(newarr); 
+
+
+// let arr = [1,2,3,4];
+// let a = arr.find(function(val){
+//     return val === 4;
+// });
+// console.log(a);
+
+// let arr = [
+//     {id : 1 , key : 1},
+//     {id : 2 , key : 2},
+//     {id : 3 , key : 3},
+//     {id : 4 , key : 4},
+// ]
+
+// let a = arr.find(function(val){
+//     return val.id === 3;
+// });
+// console.log(a); 
+
+
+// let a = ['shantanu','teena'];
+// a.splice(2,0,'sakshi');
+// console.log(a);
+
+
+
+
+// let number = [1,2,3,4,5];
+// let sq = number.map(n => n * n);
+// console.log(sq);
+
+
+//convert string to uppercase
+
+// let name = ['shantanu','teena','sanjana'];
+// let u = name.map(name => name.toUpperCase());
+// console.log(u);
+
+//extract specific propert from object
+
+// let students = [
+//   { id: "Shantanu", marks: 85 },
+//   { id: "Teena", marks: 92 },
+//   { id: "Suraksha", marks: 78 }
+// ];
+
+// let name = students.map(students => students.id);
+// console.log(name);
+
+// add index to each element
+
+// let fruits = ["apple", "banana", "cherry"];
+// let labeled = fruits.map((fruit, index) => `${index + 1}. ${fruit}`);
+// console.log(labeled); // ["1. apple", "2. banana", "3. cherry"]
