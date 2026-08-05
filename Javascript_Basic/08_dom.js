18de66f951ae63136968f76827afdfc8384252f6
Shantanu Jumale <shantanujumale92@gmail.com>
2026-08-05 21:47:14 +0530

    Add files via upload

---
+++ Javascript_Basic/08_dom.js
@@ -0,0 +1,39 @@
+console.log('Hello Teena...');
+
+// let abcd = document.querrySelector('h1');
+// console.dir(abcd);
+
+// let h1 = document.querySelector('h1');
+// console.dir(h1);
+
+// h1.textContent = 'Hello Teena';
+     
+// h1.innerHTML = 'Hey Teena...'
+
+// h1.innerHTML = '<i> hey </>';
+// h1.hidden = true;
+
+
+// let a = document.querySelector('a'); 
+// console.log(a)
+// a.href = 'https://www.google.com'; 
+
+ 
+// let h1 = document.createElement('h1'); 
+// h1.textContent = "Hello Teena";
+// document.body.append(h1);
+
+
+// let h1 = document.createElement('h1');
+// console.log(h1);
+
+
+// let h1 = document.createElement('h1');
+// h1.textContent = 'hello ji kaise hoo';
+// document.querySelector('body').append(h1);
+
+// let h1 = document.querySelector('h1');
+// h1.textContent = 'hey kaise hoo..';
+// document.querySelector('div').appendChild(h1);
+
+let h1 = document.querySelector('h1')
