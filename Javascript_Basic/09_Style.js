523591bca5ffb5d743f117a75dc16adf2466c4fa
Shantanu Jumale <shantanujumale92@gmail.com>
2026-08-05 21:47:37 +0530

    Add files via upload

---
+++ Javascript_Basic/09_Style.js
@@ -0,0 +1,65 @@
+// let h1 = document.querySelector('h1');
+// console.log(h1);
+
+let h1 = document.querySelector('h1');
+// h1.style.color = 'green';
+// h1.style.backgroundColor = 'yellow';
+
+// h1.classList.add('hulu');
+// h1.classList.remove('hulu');
+
+// let p = document.querySelector('p');
+// p.classList.remove('hulu');
+
+// p.classList.toggle('hulu');
+
+// let heading = document.querySelector("#heading");
+// heading.textContent = 'Welcome to Jungle';
+
+
+// Select all <li> elements
+// let lis = document.querySelectorAll('li');
+
+// // Using for loop
+// for (let i = 0; i < lis.length; i++) {
+//     console.log(lis[i].textContent);
+// }
+
+// Alternative: Using forEach
+// lis.forEach(function(val) {
+//     console.log(val.textContent);
+// });
+
+
+// what is the difference between innerText, textContent and innerHTML
+// when should use innerText, textContent
+// how to you get src of an image using javascript?
+// what does setAttribute() do?  ---> document.querySelector('img').setAttribute('src','https://');
+// what does createElement() do? what returned?  --> h1 = createElement('h1')
+//what the different between appendChild and prepend()
+// add hightlight class to every even item in a list
+
+
+//create a new list item <li> new task </li> and addd it to end of a </ul>
+
+// let ul = document.querySelector('ul');
+// let li = document.createElement('li');
+// li.textContent = 'New Task';
+
+// ul.appendChild(li);
+
+
+//create a new image element with placeholder source and add it at top of a div
+
+
+// let img = document.createElement('img');
+
+// img.setAttribute('src','https://th.bing.com/th/id/OIP.4A4iwzRKJEz5nWjYh8rwDwHaHa?w=172&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3');
+
+// img.classList.add('placeholder');
+// document.querySelector('div').prepend(img);
+
+
+
+
+
